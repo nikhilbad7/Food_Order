0aba22b12499a70b657033362872e9cf0a25c826
Nikhil7@ <31141190+nikhilbad7@users.noreply.github.com>
2024-01-25 03:48:03 +0530

    Update AvailableMeals.js
    New database url added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect( () => {
      const fetchMeals = async () => {
 
-     const response   = await fetch('https://react-http-c732c-default-rtdb.firebaseio.com/meals.json');
+     const response   = await fetch('https://react-recipes-e65f4-default-rtdb.firebaseio.com/meals.json');
      
      if (!response.ok) {
         throw new Error('Something went wrong!');
